0a446911bb8ccd2aa0f18ade8f23b2e6d172160f
zeil <zeil@cs.odu.edu>
2023-08-15 13:46:27 -0400

    substitution comments

--- src/main/java/edu/odu/cs/cs350/Substitutions.java
+++ src/main/java/edu/odu/cs/cs350/Substitutions.java
@@ -1,15 +1,26 @@
 package edu.odu.cs.cs350;
 
+/**
+ * A simple container for pairs of strings.
+ */
 public class Substitutions {
 
     private String key;
     private String repl;
 
+    /**
+     * Create oe substitution object
+     * @param key a string to search for
+     * @param replacement a replacement for that string, when found
+     */
     public Substitutions(String key, String replacement) {
         this.key = key;
         this.repl = replacement;
     }
 
+    /**
+     * @return the key string to search for
+     */
     public String getKey() {
         return key;
     }
